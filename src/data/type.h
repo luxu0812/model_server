9f8e46faf909d0a7bbccd91e0fb93d27a8821aa5
Xu Lu <xu.lu@shopee.com>
2023-07-19 19:00:51 +0800

    update

---
+++ src/data/type.h
@@ -0,0 +1,36 @@
+// Copyright 2023 zh.luxu1986@gmail.com
+
+#ifndef INFER_ENGINE_SRC_DATA_TYPE_H_
+#define INFER_ENGINE_SRC_DATA_TYPE_H_
+
+#include <stdint.h>
+#include <vector>
+#include <string>
+
+namespace infer_engine {
+
+struct Tensor {
+  std::string name;
+  std::vector<int32_t> shape;
+  std::vector<float> data;
+};
+
+struct Instance {
+  std::vector<Tensor> features;
+};
+
+struct Score {
+  std::vector<Tensor> scores;
+}
+
+struct BatchInstance {
+  std::vector<Instance> instances;
+};
+
+struct BatchScore {
+  std::vector<Score> scores;
+};
+
+}  // namespace infer_engine
+
+#endif  // INFER_ENGINE_SRC_DATA_TYPE_H_
