b488fdbd55147d83e6b8e0849fcdb850b058ba1b
Xu Lu <111041654+luxu0812@users.noreply.github.com>
2023-12-28 19:24:03 +0800

    Update tf_engine.cpp

--- src/engine/tf_engine.cpp
+++ src/engine/tf_engine.cpp
@@ -169,15 +169,6 @@ void TFEngine::infer(Instance *instance, Score *score) noexcept(false) {
   score_from_tensor(batch_size, output_tensors, score);
 }
 
-void TFEngine::infer(OptInstance *instance, OptScore *score) noexcept(false) {
-  std::shared_lock<std::shared_mutex> engine_lock(engine_mtx_);
-  if (!inited_) {
-    const std::string& err_msg = "[" + std::string(__FILE__) + ":" + std::to_string(__LINE__) + "]["
-      + conf_.brief() + "] " + "Engine not initialized";
-    throw std::runtime_error(err_msg);
-  }
-}
-
 void TFEngine::trace(Instance *instance, Score *score) noexcept(false) {
   std::shared_lock<std::shared_mutex> engine_lock(engine_mtx_);
   if (!inited_) {
@@ -232,15 +223,6 @@ void TFEngine::trace(Instance *instance, Score *score) noexcept(false) {
   std::ofstream ofs("trace_data_" + std::to_string(trace_data_index.load()) + ".pb");
   ofs << trace_data_str;
   ofs.close();
-}
-
-void TFEngine::trace(OptInstance *instance, OptScore *score) noexcept(false) {
-  std::shared_lock<std::shared_mutex> engine_lock(engine_mtx_);
-  if (!inited_) {
-    const std::string& err_msg = "[" + std::string(__FILE__) + ":" + std::to_string(__LINE__) + "]["
-      + conf_.brief() + "] " + "Engine not initialized";
-    throw std::runtime_error(err_msg);
-  }
 }
 
 void TFEngine::run_session(
