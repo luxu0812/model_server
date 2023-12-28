8c628f2dc9a059617d8ec25dc0e7f2ce01d5a9fe
Xu Lu <111041654+luxu0812@users.noreply.github.com>
2023-12-28 17:07:00 +0800

    Bzlmod (#2)
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * update
    
    * udpate
    
    * update
    
    * update
    
    * update
    
    * update'
    
    * update
    
    ---------
    
    Co-authored-by: Xu Lu <xu.lu@shopee.com>

--- src/engine/onnx_engine.h
+++ src/engine/onnx_engine.h
@@ -43,9 +43,11 @@ class ONNXEngine : public Engine {
 
   // Perform inference using the ONNX runtime
   void infer(Instance *instance, Score *score) noexcept(false) override;
+  void infer(OptInstance *instance, OptScore *score) noexcept(false) override;
 
   // Perform inference with trace using the ONNX runtime
   void trace(Instance *instance, Score *score) noexcept(false) override;
+  void trace(OptInstance *instance, OptScore *score) noexcept(false) override;
 
  protected:
   // Load the TensorFlow graph from the .pb file
