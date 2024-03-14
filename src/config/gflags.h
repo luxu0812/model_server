c0fbecd89b70047d01baa92cc524de8a3ab3304a
Xu Lu <111041654+luxu0812@users.noreply.github.com>
2024-03-14 18:24:30 +0800

    Update gflags.h

--- src/config/gflags.h
+++ src/config/gflags.h
@@ -3,24 +3,27 @@
 #ifndef MODEL_SERVER_SRC_CONFIG_GFLAGS_H_
 #define MODEL_SERVER_SRC_CONFIG_GFLAGS_H_
 
-#include "gflags/gflags.h"
+#include <stdint.h>
+#include <string>
+#include "absl/flags/flag.h"
+#include "absl/flags/declare.h"
 
-DECLARE_string(host);
-DECLARE_int32(port);
+ABSL_DECLARE_FLAG(std::string, host);
+ABSL_DECLARE_FLAG(int32_t, port);
 
-DECLARE_int32(number_of_inference_workers);
-DECLARE_int32(batch_size);
+ABSL_DECLARE_FLAG(int32_t, number_of_inference_workers);
+ABSL_DECLARE_FLAG(int32_t, batch_size);
 
-DECLARE_string(local_model_dir);
-DECLARE_int32(number_of_test_cases);
-DECLARE_int32(number_of_producers);
-DECLARE_int32(number_of_consumers);
+ABSL_DECLARE_FLAG(std::string, local_model_dir);
+ABSL_DECLARE_FLAG(int32_t, number_of_test_cases);
+ABSL_DECLARE_FLAG(int32_t, number_of_producers);
+ABSL_DECLARE_FLAG(int32_t, number_of_consumers);
 
-DECLARE_int32(engine_opt_level);
-DECLARE_int32(engine_jit_level);
-DECLARE_int32(engine_inter_op_parallelism_threads);
-DECLARE_int32(engine_intra_op_parallelism_threads);
-DECLARE_bool(engin_use_global_thread_pool);
-DECLARE_bool(engine_ort_parrallel_execution);
+ABSL_DECLARE_FLAG(int32_t, engine_opt_level);
+ABSL_DECLARE_FLAG(int32_t, engine_jit_level);
+ABSL_DECLARE_FLAG(int32_t, engine_inter_op_parallelism_threads);
+ABSL_DECLARE_FLAG(int32_t, engine_intra_op_parallelism_threads);
+ABSL_DECLARE_FLAG(bool, engin_use_global_thread_pool);
+ABSL_DECLARE_FLAG(bool, engine_ort_parrallel_execution);
 
 #endif  // MODEL_SERVER_SRC_CONFIG_GFLAGS_H_
